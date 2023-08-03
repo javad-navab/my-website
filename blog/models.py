4b7f1cf488a66245433de06c92c358464297733c
javad <javadnavabzadeh@yahoo.com>
2023-08-03 20:31:21 +0330

    first models
    first models of website has been created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
 # Create your models here.
+
+class post(models.Model):
+    #image
+    #author
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    #tags
+    #category
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_at = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True )
