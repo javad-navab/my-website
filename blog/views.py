866b9f0a95451037a1735fb3b87376c5d042a5a9
javad <javadnavabzadeh@yahoo.com>
2023-08-10 00:12:56 +0330

    bug fix
    new changes has been done and bugs are fixed

--- blog/views.py
+++ blog/views.py
@@ -3,20 +3,23 @@ from blog.models import post
 import datetime
 
 
+
 # Create your views here.
 def blog(request ):
-    yesterday = datetime.date.fromordinal(datetime.date.today().toordinal()-1)
-    posts = post.objects.filter(published_at__lte=yesterday)
+    t = datetime.datetime.now()
+    posts = post.objects.filter(published_at__lte=t , status=True)
     context = {'posts' : posts }
     return render(request,'blog/blog-home.html' , context)
 
 def blog_single(request , pid):
-    counter=post.objects.get(id=pid)
-    counter.counted_views=counter.counted_views+1
-    counter.save()
+    # counter=post.objects.get(id=pid)
+    # counter.counted_views=counter.counted_views+1
+    # counter.save()
     posts = post.objects.get(id=pid)
+    posts.counted_views=posts.counted_views+1
+    posts.save()
     posts = get_object_or_404(post,pk=pid)
-    context = {'post':posts ,'counter':counter}
+    context = {'post':posts }
     return render(request,'blog/blog-single.html' , context)
 
 # def test(request , pid): 
